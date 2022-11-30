47fdc52ab2b2bd79be484c665ad175717f2c5ac4
jbajr1982 <98054949+jbajr1982@users.noreply.github.com>
2022-11-29 22:47:58 -0300

    Update flaskapp.py

--- flaskapp.py
+++ flaskapp.py
@@ -1,11 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from sqlite3 import connect
+from pandas import read_sql_query
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-	return 'Hello World bla bla'
+@app.route('/', methods = ['POST', 'GET'])
 
-# main driver function
+def gfg():
+    con = connect("database.db")
+    cur = con.cursor()
+    dt_av = read_sql_query("SELECT * FROM parametros", con).iloc[0,1]
+    if request.method == 'POST':
+        dt_av = request.form['ht_dtav']
+        cur.execute(f"UPDATE parametros SET DT_AVAL = '{dt_av}' WHERE NR = 1" )
+        con.commit()
+        return render_template("home.html", dt_av = dt_av )    
+    return render_template("home.html", dt_av = dt_av)    
+
+@app.route('/', methods = ['POST', 'GET'])
+
+def f_salv():
+    ht_dtav = request.form['ht_dtav']
+    return render_template("home.html", ht_dtav = ht_dtav)    
+
 if __name__ == '__main__':
-	app.run()
+   app.run(debug = True)
